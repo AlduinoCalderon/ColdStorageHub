571035f2699b8aa3f322b92bdede121ad67f8091
AlduinoCalderon <alduinocalderon@gmail.com>
2025-04-02 12:32:16 +0900

    feat: innerjoin endpoit for storage units-wh

--- src/api/mysql/routes/storage-unit.routes.js
+++ src/api/mysql/routes/storage-unit.routes.js
@@ -1,3 +1,5 @@
+// Actualizar el archivo: src/api/mysql/routes/storage-unit.routes.js
+
 const express = require('express');
 const router = express.Router();
 const storageUnitController = require('../controllers/storage-unit.controller');
@@ -5,6 +7,7 @@ const storageUnitController = require('../controllers/storage-unit.controller');
 // Rutas para unidades de almacenamiento
 router.get('/', storageUnitController.getAllStorageUnits);
 router.get('/stats/:warehouseId', storageUnitController.getStorageUnitStats);
+router.get('/wh/:id', storageUnitController.getStorageUnitsByWarehouseId); // Nueva ruta
 router.get('/:id', storageUnitController.getStorageUnitById);
 router.post('/', storageUnitController.createStorageUnit);
 router.put('/:id', storageUnitController.updateStorageUnit);
