c0b9bc20e17538354573d5874202cd7fcaec9090
AlduinoCalderon <alduinocalderon@gmail.com>
2025-04-05 03:02:06 +0900

    fix: PUT endponit to update dht info at mysql db

--- src/index.js
+++ src/index.js
@@ -87,7 +87,7 @@ async function processReadingsBuffer() {
 
     try {
         // Enviar datos a la API
-        const response = await fetch('https://coldstoragehub.onrender.com/API/storage-unit/1', {
+        const response = await fetch('https://coldstoragehub.onrender.com/API/storage-units/1', {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
