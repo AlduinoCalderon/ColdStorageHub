cbeca83462621ce654a21a06bcb5f88786c8f962
AlduinoCalderon <aldocalderon97@gmail.com>
2025-04-05 22:28:18 +0900

    fix: Arduino script

--- MQTT_M5Stack.ino
+++
@@ -1,4 +0,0 @@
-const char* mqtt_server = "chameleon.lmq.cloudamqp.com";
-const int mqtt_port = 8883;
-const char* mqtt_user = "ylltduke:ylltduke";
-const char* mqtt_password = "0l2ahZ_tmYsp7Dt57IOJU8NcbccV5tw9"; 
